55d724418403e3b5d966ffad9687194a248245b7
itsRiyaGupta <riyaruchi139@gmai.com>
2022-11-18 19:12:23 +0530

    Completed the final styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./componenets/header";
+import Body from "./componenets/body";
+import Footer from "./componenets/footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen bg-blue-100 flex justify-center">
+      <div className=" bg-white w-96 p-4 my-2 rounded-lg">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
     </div>
   );
 }
